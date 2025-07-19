a628248833862915410d7f099ee2ef6dfc166bd2
Maksooda <sayyedmaksooda@gmail.com>
2025-07-19 20:05:19 +0530

    App.js

--- App.js
+++ App.js
@@ -19,6 +19,3 @@ const styles = StyleSheet.create({
   },
 });
 
-//https://xzmypkblyosjixknwhky.supabase.co
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6bXlwa2JseW9zaml4a253aGt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE1OTM0MTcsImV4cCI6MjAyNzE2OTQxN30.-MwqUV6A5jOIWQ2MHXaIenQ8navSVk-sWgTxRLHfqp4
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6bXlwa2JseW9zaml4a253aGt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE1OTM0MTcsImV4cCI6MjAyNzE2OTQxN30.-MwqUV6A5jOIWQ2MHXaIenQ8navSVk-sWgTxRLHfqp4
